5f19d6163d6c2273f8207cdbc033da814b6b0cab
Rycbar123 <71559822+xx025@users.noreply.github.com>
2023-12-17 11:39:57 +0800

    Update auto-reply.js
    盘友社区 更换了 xyz 域名

--- panyoubbs_com/auto-reply.js
+++ panyoubbs_com/auto-reply.js
@@ -5,11 +5,13 @@
 // @description  盘友社区自动回复帖子，并自动删除内容，基于协议无痕无打打扰
 // @author       xx025
 // @homepage     https://github.com/xx025/strawberry
-// @match        https://www.panyoubbs.com/thread/*
+// @match        https://panyoubbs.xyz/thread/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=panyoubbs.com
 // @require      https://cdn.staticfile.org/jquery-cookie/1.4.1/jquery.cookie.min.js
 // @run-at       document-idle
 // @grant        none
+// @downloadURL https://update.greasyfork.org/scripts/463957/%E7%9B%98%E5%8F%8B%E7%A4%BE%E5%8C%BA%E8%87%AA%E5%8A%A8%E5%9B%9E%E5%A4%8D.user.js
+// @updateURL https://update.greasyfork.org/scripts/463957/%E7%9B%98%E5%8F%8B%E7%A4%BE%E5%8C%BA%E8%87%AA%E5%8A%A8%E5%9B%9E%E5%A4%8D.meta.js
 // ==/UserScript==
 
 
@@ -19,7 +21,7 @@ if (undefined !== $.cookie('think_uid')) {
 
     //检查是否是在帖子页面
     //通过url判断 https://www.panyoubbs.com/thread/*.html
-    if (document.location.href.search('https://www.panyoubbs.com/thread/') > -1) {
+    if (document.location.href.search('https://panyoubbs.xyz/thread/') > -1) {
         //检查当前帖子是否需要回复可见
         let jm = $('.jm')
         if (jm.length > 0) {
