93f1c6740d9590148f4105c5297db48f23b16112
xx025 <71559822+xx025@users.noreply.github.com>
2023-12-17 12:46:38 +0800

    Create github.js

---
+++ demo/github.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Github Js Demo
+// @namespace    http://tampermonkey.net/
+// @version      2023-12-17
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+// 弹出 hello world!
+alert("hello world!");
