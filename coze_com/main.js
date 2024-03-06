aa7d1563acd84b28c2e376c91bf1026745a7340f
Rycbar123 <71559822+xx025@users.noreply.github.com>
2024-03-06 16:37:20 +0800

    增加名称关键词

--- coze_com/main.js
+++ coze_com/main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Beautiful Coze| Coze 聊天面板美化
+// @name         Beautiful Coze| Coze 聊天面板美化 |免费GPT4
 // @namespace    http://tampermonkey.net/
 // @version      0.0.3
-// @description   Coze 聊天面板美化| 提示栏和插件栏的切换| 聊天面板全屏| Coze chat panel beautification| Switch between prompt bar and plugin bar| Full screen chat panel
+// @description  Coze 聊天面板美化| 提示栏和插件栏的切换| 聊天面板全屏| Coze chat panel beautification| Switch between prompt bar and plugin bar| Full screen chat panel
 // @author       xx025
 // @homepage     https://github.com/xx025/strawberry
 // @match        https://www.coze.com/*
